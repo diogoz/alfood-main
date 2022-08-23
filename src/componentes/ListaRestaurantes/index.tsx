8d60b625efe7385a5eca236d877657a255a2ad19
diogoevangelista <dhesousa77@gmail.com>
2022-08-23 17:10:08 -0300

    Paginação tratada

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,20 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { IPaginacao } from "../../interfaces/IPaginacao";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState("");
 
   useEffect(() => {
     //obter restaurantes
-    axios.get("http://localhost:8000/api/v1/restaurantes/").then((response) => {
-      const { data } = response;
-      console.log(response.data.results);
-      setRestaurantes(data.results);
-    });
+    axios
+      .get<IPaginacao<IRestaurante>>(
+        "http://localhost:8000/api/v1/restaurantes/"
+      )
+      .then((response) => {
+        const { data } = response;
+        setRestaurantes(data.results);
+        setProximaPagina(data.next);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
   }, []);
+
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina).then((response) => {
+      const { data } = response;
+      setRestaurantes([...restaurantes, ...data.results]);
+      setProximaPagina(data.next);
+    });
+  };
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -24,6 +41,7 @@ const ListaRestaurantes = () => {
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
+      {proximaPagina && <button onClick={verMais}>ver mais</button>}
     </section>
   );
 };
