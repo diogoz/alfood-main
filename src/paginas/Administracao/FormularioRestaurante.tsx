dd9e8f6d57531c51e02a35b0a2a22318616b2b2e
diogoevangelista <dhesousa77@gmail.com>
2022-08-24 17:59:33 -0300

    Editando restaurantes

--- src/paginas/Administracao/FormularioRestaurante.tsx
+++ src/paginas/Administracao/FormularioRestaurante.tsx
@@ -1,20 +1,42 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../interfaces/IRestaurante";
 
 const FormularioRestaurante = () => {
   const [nomeRestaurante, setNomeRestaurante] = useState("");
+  const parametros = useParams();
+  useEffect(() => {
+    if (parametros.id) {
+      axios
+        .get<IRestaurante>(
+          `http://localhost:8000/api/v2/restaurantes/${parametros.id}/`
+        )
+        .then((resposta) => setNomeRestaurante(resposta.data.nome));
+    } else {
+    }
+  }, [parametros]);
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
-
-    axios
-      .post("http://localhost:8000/api/v2/restaurantes/", {
-        nome: nomeRestaurante,
-      })
-      .then(() => {
-        alert("Restaurante Cadastrado com Sucesso");
-      });
+    if (parametros.id) {
+      axios
+        .put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
+        .then(() => {
+          alert("Restaurante Atualizado com Sucesso");
+        });
+    } else {
+      axios
+        .post("http://localhost:8000/api/v2/restaurantes/", {
+          nome: nomeRestaurante,
+        })
+        .then(() => {
+          alert("Restaurante Cadastrado com Sucesso");
+        });
+    }
   };
 
   return (
